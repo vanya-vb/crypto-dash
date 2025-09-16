9c4d3716539b6f324309c76e45b1a861cc8e47ae
vanya-vb <vbozadzhieva29@gmail.com>
2025-09-16 21:00:53 +0300

    refactor fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,36 @@ import { useState, useEffect } from "react";
 const API_URL =
     'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
-function App() {
-    const [coins, setCoins] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    function App() {
+        const [coins, setCoins] = useState([]);
+        const [loading, setLoading] = useState(true);
+        const [error, setError] = useState(null);
 
-    useEffect(() => {
-        fetch(API_URL)
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error('Failed to fetch data');
+        useEffect(() => {
+            const fetchCoins = async () => {
+                try {
+                    const res = await fetch(API_URL);
+
+                    if (!res.ok) {
+                        throw new Error('Failed to fetch data');
+                    }
+
+                    const data = await res.json();
+                    console.log(data);
+                    setCoins(data);
+                } catch (err) {
+                    setError(err.message);
+                } finally {
+                    setLoading(false);
                 }
+            }
 
-                return res.json();
-            })
-            .then((data) => {
-                console.log(data);
-                setCoins(data);
-                setLoading(false);
-            })
-            .catch((err) => {
-                setError(err.message);
-                setLoading(false);
-            });
-    }, []);
+            fetchCoins();
+        }, []);
 
-    return (
-        <h1>🚀 Crypto Dash</h1>
-    )
-}
+        return (
+            <h1>🚀 Crypto Dash</h1>
+        )
+    }
 
 export default App
