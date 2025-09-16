3130572f199325ebc544ae565d5070ab21cbe952
vanya-vb <vbozadzhieva29@gmail.com>
2025-09-16 21:55:11 +0300

    extract coin card into separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { ClipLoader } from "react-spinners";
 
+import CoinCard from "./components/CoinCard";
+
 const API_URL =
     // 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
@@ -54,20 +56,7 @@ const API_URL =
                 {!loading && !error && (
                     <main className="grid">
                         {coins.map(coin => (
-                            <div className="coin-card" key={coin.id}>
-                                <div className="coin-header">
-                                    <img src={coin.image} alt={coin.name} className="coin-image" />
-                                    <div>
-                                        <h2>{coin.name}</h2>
-                                        <p className="symbol">{coin.symbol.toUpperCase()}</p>
-                                    </div>
-                                </div>
-                                <p>Price: ${coin.current_price.toLocaleString()}</p>
-                                <p className={coin.price_change_percentage_24h >= 0 ? 'positive' : 'negative'}>
-                                    {coin.price_change_percentage_24h.toFixed(2)} %
-                                </p>
-                                <p>Market Cap: {coin.market_cap.toLocaleString()}</p>
-                            </div>
+                            <CoinCard key={coin.id} coin={coin} />
                         ))}
                     </main>
                 )}
