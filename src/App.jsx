b490ce42945af9ffaeac7cd0952db9052076637a
vanya-vb <vbozadzhieva29@gmail.com>
2025-09-16 20:48:08 +0300

    add css file and fetch crypto data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react";
+
+const API_URL =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+
 function App() {
+    const [coins, setCoins] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch(API_URL)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+
+                return res.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setCoins(data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                setError(err.message);
+                setLoading(false);
+            });
+    }, []);
 
     return (
-        <h1>Crypto Dash</h1>
+        <h1>🚀 Crypto Dash</h1>
     )
 }
 
