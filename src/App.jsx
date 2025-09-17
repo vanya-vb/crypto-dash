ca8ea2be3ca9cc4ac7c31c696a86bf18e28d3539
vanya-vb <vbozadzhieva29@gmail.com>
2025-09-17 09:43:43 +0300

    add home page and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
-import { ClipLoader } from "react-spinners";
+import { Route, Routes } from "react-router";
 
-import CoinCard from "./components/CoinCard";
-import LimitSelector from "./components/LimitSelector";
-import FilterInput from "./components/FilterInput";
-import SortSelector from "./components/SortSelector";
+import HomePage from "./pages/HomePage";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -37,65 +34,23 @@ function App() {
         fetchCoins();
     }, [limit]);
 
-    const override = {
-        margin: '200px auto',
-    };
-
-    const filteredCoins = coins.filter(coin => {
-        return (
-            coin.name.toLowerCase().includes(filter.toLowerCase()) ||
-            coin.symbol.toLowerCase().includes(filter.toLowerCase())
-        )
-    })
-        .slice()
-        .sort((a, b) => {
-            switch (sortBy) {
-                case 'market_cap_desc': return b.market_cap - a.market_cap;
-                case 'market_cap_asc': return a.market_cap - b.market_cap;
-                case 'price_desc': return b.current_price - a.current_price;
-                case 'price_asc': return a.current_price - b.current_price;
-                case 'change_desc': return b.price_change_percentage_24h - a.price_change_percentage_24h;
-                case 'change_asc': return a.price_change_percentage_24h - b.price_change_percentage_24h;
-            }
-        })
 
     return (
-        <div>
-            <h1>🚀 Crypto Dash</h1>
-            {
-                loading && <ClipLoader
-                    color='grey'
+        <Routes>
+            <Route path="/" element={
+                <HomePage
+                    coins={coins}
+                    filter={filter}
+                    setFilter={setFilter}
+                    limit={limit}
+                    setLimit={setLimit}
+                    sortBy={sortBy}
+                    setSortBy={setSortBy}
                     loading={loading}
-                    cssOverride={override}
-                    size={120}
-                    aria-label="Loading Spinner"
-                    data-testid="loader"
+                    error={error}
                 />
-            }
-
-            {error && <div className="error">{error}</div>}
-
-            <div className="top-controls">
-                <FilterInput filter={filter} onFilterChange={setFilter} />
-                <LimitSelector limit={limit} onLimitChange={setLimit} />
-                <SortSelector sortBy={sortBy} onSortChange={setSortBy} />
-            </div>
-
-            {!loading && !error && (
-                <main className="grid">
-                    {
-                        filteredCoins.length > 0 ?
-                            (
-                                filteredCoins.map(coin => (
-                                    <CoinCard key={coin.id} coin={coin} />
-                                ))
-                            )
-                            :
-                            (<p>No matching coins</p>)
-                    }
-                </main>
-            )}
-        </div>
+            } />
+        </Routes>
     )
 }
 
